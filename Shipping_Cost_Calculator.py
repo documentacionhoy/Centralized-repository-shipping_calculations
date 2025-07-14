14e437c78c56fc6f9d7ab491f1f8b5afe0928d1e
Daniel Muñoz Egido <daniel.megido@guibib.com>
2025-07-14 19:35:47 +0200

    agregada línea adicional a Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,3 +11,5 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
  # Aquí hay una nueva actualización por <documentacionhoy>
+
+# Here is another update by <documentacionhoy>
